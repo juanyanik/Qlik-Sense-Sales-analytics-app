3062a7c645739431b624f19a328f3321742426a3
juanyanik <100418934+juanyanik@users.noreply.github.com>
2023-03-04 19:23:23 -0300

    Add files via upload

---
+++ Sales_Product_Analysis_Mashup/Sales_Product_Analysis_Mashup.js
@@ -0,0 +1,40 @@
+/*
+ * Basic responsive mashup template
+ * @owner Enter you name here (xxx)
+ */
+/*
+ *    Fill in host and port for Qlik engine
+ */
+var prefix = window.location.pathname.substr( 0, window.location.pathname.toLowerCase().lastIndexOf( "/extensions" ) + 1 );
+var config = {
+	host: window.location.hostname,
+	prefix: prefix,
+	port: window.location.port,
+	isSecure: window.location.protocol === "https:"
+};
+require.config( {
+	baseUrl: ( config.isSecure ? "https://" : "http://" ) + config.host + (config.port ? ":" + config.port : "") + config.prefix + "resources"
+} );
+
+require( ["js/qlik"], function ( qlik ) {
+	qlik.on( "error", function ( error ) {
+		$( '#popupText' ).append( error.message + "<br>" );
+		$( '#popup' ).fadeIn( 1000 );
+	} );
+	$( "#closePopup" ).click( function () {
+		$( '#popup' ).hide();
+	} );
+
+	//callbacks -- inserted here --
+	//open apps -- inserted here --
+	var app = qlik.openApp('[JuanY]Gestión de ventas.qvf', config);
+
+	//get objects -- inserted here --
+	app.getObject('QV04','qchfRm');
+	app.getObject('QV02','FJyaxvQ');
+	app.getObject('QV03','YXSbfjQ');
+	
+	app.getObject('QV01','5a7b9c31-d174-4f0f-aa92-2228ee217873');
+	//create cubes and lists -- inserted here --
+
+} );
